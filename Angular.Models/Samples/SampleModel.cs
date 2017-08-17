b9885f7de935dc2adfcb300b91ffd209b3927423
ar3a51 <ar3a51@yahoo.com>
2017-08-18 08:11:01 +1000

    Modify the sample model

--- Angular.Models/Samples/SampleModel.cs
+++ Angular.Models/Samples/SampleModel.cs
@@ -8,9 +8,14 @@ namespace Angular.Models.Samples
 {
   public class SampleModel : ISQLMapper
   {
+    
     public string col1;
     public string col2;
     public string col3;
+
+    public int SampleID;
+
+    public int Identity { set => SampleID = value; }
 
     public SQLQuery InsertStatement()
     {
@@ -37,7 +42,20 @@ namespace Angular.Models.Samples
     {
       SQLQuery query = new SQLQuery();
 
-      query.Query = @"";
+      query.Query = @"
+        UPDATE Sample
+            set col1 = @col1,
+            set col2 = @col2,
+            set col3 = @col3
+        WHERE SampleID = @SampleID
+      ";
+
+      query.Params = new List<IDbDataParameter>() {
+        new SqlParameter() { ParameterName = "@col1", Value = col1, DbType=DbType.String, Direction = ParameterDirection.Input },
+        new SqlParameter() { ParameterName = "@col2", Value = col2, DbType=DbType.String, Direction = ParameterDirection.Input },
+        new SqlParameter() { ParameterName = "@col3", Value = col3, DbType=DbType.String, Direction = ParameterDirection.Input },
+        new SqlParameter() { ParameterName = "@ID", DbType = DbType.Int32, Direction = ParameterDirection.Input, Value = SampleID}
+      };
 
       return query;
     }
